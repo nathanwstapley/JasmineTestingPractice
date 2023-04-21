3098fe6690b65e0ffb7ae4bfecf6c3b60783ff8b
Nathan W. Stapley <nathanwstapley@gmail.com>
2023-04-21 10:22:18 -0400

    learned how to use jasmine to test functions written in my javascript code

--- taxes.test.js
+++ taxes.test.js
@@ -1,6 +1,7 @@
 it('should calculate lower-bracket taxes', function () {
     expect(calculateTaxes(10000)).toEqual(1500);
     expect(calculateTaxes(20000)).toEqual(3000);
+    expect(calculateTaxes(27500)).toEqual(4125);
   });
   
   it('should calculate higher-bracket taxes', function () {
